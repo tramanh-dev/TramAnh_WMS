38bbff1c4dd0b77de951e5d026e1fe49603ca00a
Tram Anh <trammanh278@gmail.com>
2026-05-04 11:06:37 +0700

    add staffid

--- Migrations/20260504040505_AddStaffIdToOrder.Designer.cs
+++ Migrations/20260504040505_AddStaffIdToOrder.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TramAnh_WMS.Data;
@@ -11,9 +12,11 @@ using TramAnh_WMS.Data;
 namespace TramAnh_WMS.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260504040505_AddStaffIdToOrder")]
+    partial class AddStaffIdToOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260504040505_AddStaffIdToOrder.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TramAnh_WMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStaffIdToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StaffId",
+                table: "Orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StaffId",
+                table: "Orders",
+                column: "StaffId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Staff_StaffId",
+                table: "Orders",
+                column: "StaffId",
+                principalTable: "Staff",
+                principalColumn: "StaffId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Staff_StaffId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_StaffId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "StaffId",
+                table: "Orders");
+        }
+    }
+}
